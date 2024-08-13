3ef67edec6f22bead24b317dd5dfbbdd317f6d62
currychoco <godqhrgka0000@naver.com>
2024-08-13 23:35:19 +0900

    project initialize

---
+++ src/main/java/com/currychoco/board/HomeController.java
@@ -0,0 +1,18 @@
+package com.currychoco.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/next")
+    public String next() {
+        return "next";
+    }
+}
---
+++ src/main/java/com/currychoco/board/TestController.java
@@ -0,0 +1,15 @@
+package com.currychoco.board;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testApi() {
+        return "Test API";
+    }
+
+
+}
